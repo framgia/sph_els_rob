89b5d240ae63fd5dc005bde732bc7a194d4af53e
sun-asterisk <sun-asterisk@z217096.dynamic.ppp.asahi-net.or.jp>
2022-01-31 18:20:52 +0800

    [SELS-TASK][UI] Login (implemented comments)

--- front-end/e-learning/src/components/App.js
+++ front-end/e-learning/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Login from './Login';
 
 class App extends React.Component {
@@ -6,7 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-          <Login />
+        <Login />
       </div>
     );
   }
--- front-end/e-learning/src/components/Login.js
+++ front-end/e-learning/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+
+import Header from './Header';
+
 import Button from '@mui/material/Button';
-import './Login.css';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
@@ -10,90 +12,88 @@ import Link from '@mui/material/Link';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
-import Header from './Header';
-
 const Login = () =>  {
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
     
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const onLogin = (e) =>{
     
-    const onLogin = (e) =>{
-        e.preventDefault();
-        console.log(email);
+		e.preventDefault();
+    	console.log(email);
     };
-        return (
-            <div>
-                <Header />
-                <Container maxWidth="xs" >
-                    <Box
-                        onSubmit = {onLogin}
-                        sx={{
-                        marginTop: 10,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        }} >
-                        <Typography component="h1" variant="h5">
-                            Log in
-                        </Typography>
-                        <form>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                                onChange={e => setEmail(e.target.value)}
-                                />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                                onChange={e => setPassword(e.target.value)}
-                            />
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
-                            <Button
-                                onClick={onLogin}
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 2, mb: 2, bgcolor: '#05386b', '&:hover': {
-                                    backgroundColor: '#edf5e1',
-                                    color: 'black'
-                                }, }}>
-                                LOG IN
-                            </Button>
-                        </form>
-                                {email} {password}
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Container>
-            </div>
-        );
+    
+		return (
+      <div>
+        <Header />
+          <Container maxWidth="xs" >
+            <Box
+              onSubmit = {onLogin}
+              sx={{
+                marginTop: 10,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}>
+              <Typography component="h1" variant="h5">
+                Log in
+              </Typography>
+              <form>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  onClick={onLogin}
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 2, mb: 2, bgcolor: '#05386b',
+										'&:hover': {
+                      backgroundColor: '#edf5e1',
+                      color: 'black'
+                      },}}>
+                  LOG IN
+                </Button>
+              </form>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+ 	              </Grid>
+                <Grid item>
+                	<Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Container>
+      	</div>
+      );
     }
 
 export default Login;
