2123774e074b6840847698d35084563e15836655
sun-asterisk <sun-asterisk@sun-asterisks-MacBook-Air.local>
2022-02-03 15:19:19 +0800

    [SELS-TASK][BE] Login (updated request)

--- front-end/e-learning/src/actions/index.js
+++ front-end/e-learning/src/actions/index.js
@@ -3,16 +3,13 @@
 import axios from "../apis/api";
 
 export const logIn = formValues => async dispatch => {
-  console.log(formValues);
-  var response = null;
   try {
-    response = await axios.post("/api/login", formValues);
+    const response = await axios.post("/api/login", formValues);
     dispatch({
       type: "LOG_IN",
       payload: response.data,
     });
   } catch (e) {
-    console.log(response)
     dispatch({
       type: "LOG_IN",
       payload: { error_message: "Unauthorized user!" },
