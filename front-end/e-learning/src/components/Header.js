eedf9fefab0f03488b9d1cef4da059d01e943359
sun-asterisk <sun-asterisk@sun-asterisks-MacBook-Air.local>
2022-02-03 14:07:08 +0800

    [SELS-TASK][UI] Login (applied Prettier)

--- front-end/e-learning/src/components/Header.js
+++ front-end/e-learning/src/components/Header.js
@@ -1,46 +1,47 @@
-import React from 'react';
+/** @format */
 
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
+import React from "react";
 
-const Header = () =>{
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 
-	return(
-  	<AppBar
-      position="static"
-      color="default"
+const Header = () => {
+  return (
+    <AppBar
+      position='static'
+      color='default'
       elevation={0}
-      sx={{ bgcolor: '#05386b', color: 'white'}}
+      sx={{ bgcolor: "#05386b", color: "white" }}
     >
-      <Toolbar sx={{ flexWrap: 'wrap' }}>
-				<Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-					E-Learning
-				</Typography>
-				<nav>
-					<Link
-						variant="button"
-						color="text.primary"
-						href="#"
-						underline="none"
-						sx={{ my: 1, mx: 1.5, color:'white'}}
-					>
-						Sign Up
-					</Link>
-					<Link
-						variant="button"
-						color="text.primary"
-						href="#"
-						underline="none"
-						sx={{ my: 1, mx: 1.5, color:'white'}}
-					>
-						Log In
-					</Link>
-				</nav>
-    	</Toolbar>
-  	</AppBar>
+      <Toolbar sx={{ flexWrap: "wrap" }}>
+        <Typography variant='h6' color='inherit' noWrap sx={{ flexGrow: 1 }}>
+          E-Learning
+        </Typography>
+        <nav>
+          <Link
+            variant='button'
+            color='text.primary'
+            href='#'
+            underline='none'
+            sx={{ my: 1, mx: 1.5, color: "white" }}
+          >
+            Sign Up
+          </Link>
+          <Link
+            variant='button'
+            color='text.primary'
+            href='#'
+            underline='none'
+            sx={{ my: 1, mx: 1.5, color: "white" }}
+          >
+            Log In
+          </Link>
+        </nav>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
 
 export default Header;
