aa0854faab4292fb8d566f3cac14c8da3150725e
sun-asterisk <sun-asterisk@sun-asterisks-MacBook-Air.local>
2022-02-11 14:50:04 +0800

    [SELS-TASK][BE] Login v2

---
+++ front-end/e-learning/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+
+const App = () => {
+  // const [token, setToken] = useState('')
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
