6ed5501993a74d88b55f50be3cf3af10f76fea7f
rdelrosariojr <roberto.delrosario@sun-asterisk.com>
2022-03-02 10:03:03 +0800

    [SELS-TASK][UI/BE] Category Display (rebased to master)

--- front-end/e-learning/src/components/categories/Category.js
+++ front-end/e-learning/src/components/categories/Category.js
@@ -1,44 +1,72 @@
 import { connect } from "react-redux";
 import React, { useEffect } from "react";
-import { useNavigate } from 'react-router-dom'; 
-import { useCookies } from 'react-cookie';
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 import Header from "../Header";
 import Create from "./Create";
 import List from "./List";
+import { bgcolor } from "@mui/system";
 
-const Category = ({auth}) =>{
-  const [cookies, setCookie] = useCookies(['user']);
+const Category = ({ auth }) => {
+  const [cookies, setCookie] = useCookies(["user"]);
 
   let navigate = useNavigate();
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
   useEffect(() => {
-    if(cookies.token === undefined){
-      navigate('/');
+    if (cookies.token === undefined) {
+      navigate("/");
     }
   }, [auth]);
-  return(
+
+  return (
     <div>
-      <Header title="Admin" isLogin="true" />
-      <Container maxWidth="lg">
+      <Header title="Admin" />
+      <Container maxWidth="xl">
         <Box
           sx={{
             marginTop: 3,
             display: "flex",
-            justifyContent: 'flex-end',
+            justifyContent: "flex-end",
           }}
         >
-          <Create token={cookies.token} user={cookies.user} />
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Typography variant="h4" color="inherit">
+                Categories
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              sx={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Create token={cookies.token} user={cookies.user} />
+            </Grid>
+          </Grid>
         </Box>
       </Container>
       <List token={cookies.token} user={cookies.user} />
     </div>
   );
-}
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     auth: state.auth,
   };
